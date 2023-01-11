4503c487207f88d5f6a203ee1198cd860e7fe552
Rafael Kiss <rafaelskiss1@hotmail.com>
2023-01-11 20:25:29 -0300

    First commit

---
+++ loja/src/main/java/br/com/alura/loja/Produto.java
@@ -0,0 +1,4 @@
+package br.com.alura.loja;
+
+public class Produto {
+}
---
+++ loja/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.loja;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProdutoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
